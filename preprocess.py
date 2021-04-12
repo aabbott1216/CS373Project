f5f047602afbc5a625f847c355758fb5cf4ab7f2
Mark Gindling <markgindling@gmail.com>
2021-04-12 12:27:32 -0400

    Modified CSV read.

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
 
-heart = pd.read_csv('heart.csv', delimiter=',')
+# Reads in CSV file using Pandas.
+heart = pd.read_csv('resources/heart_dataset.csv', delimiter=',')
+
+# Randomizes the original CSV file so that positive and negative labels are not grouped together.
 indexlist = np.random.choice(303,303,replace = False)
 heartrandom = heart.copy()
 for i in range(303):
     heartrandom.iloc[i,:] = heart.iloc[indexlist[i],:]
-
