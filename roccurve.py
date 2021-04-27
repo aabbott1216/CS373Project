12645144f79ef7554e58c77ac5a9af870771139e
everet12 <54336351+everet12@users.noreply.github.com>
2021-04-27 16:59:54 -0400

    Update roccurve.py
    cleaned up the graph

--- roccurve.py
+++ roccurve.py
@@ -1,7 +1,14 @@
 import matplotlib.pyplot as plt
 
-
 def run(sensitivity_svm, specificity_svm, sensitivity_tree, specificity_tree):
-    plt.plot(specificity_svm, sensitivity_svm)
-    plt.plot(specificity_tree, sensitivity_tree)
+    for i in range(len(specificity_svm)):
+        specificity_svm[i]=1-specificity_svm[i]
+    for i in range(len(specificity_tree)):
+        specificity_tree[i]=1-specificity_tree[i]
+    plt.plot(specificity_svm, sensitivity_svm, color="red", label="SVM")
+    plt.plot(specificity_tree, sensitivity_tree, color="blue", label="Decision Tree")
+    plt.plot([0, 1], [0, 1], "k--", label="Random")
+    plt.xlabel("1 - Specificity")
+    plt.ylabel("Sensitivity")
+    plt.legend()
     plt.show()
