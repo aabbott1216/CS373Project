528740ba52bace623dae1c50d487428daa93e35f
Mark Gindling <markgindling@gmail.com>
2021-04-28 13:25:46 -0400

    Minor fix to HVA

--- HyperVsAcc.py
+++ HyperVsAcc.py
@@ -24,7 +24,7 @@ def run(hyperSvm, accSvm, hyperTree, accTree):
         
     fig1, (axis1, axis2) = plt.subplots(1, 2)
     fig1.suptitle('Hyperparameter vs. Accuracy')
-    axis1.plot(hyperSVM, accSVM, color="blue", linestyle="-.")
+    axis1.plot(hyperSvm, accSvm, color="blue", linestyle="-.")
     axis1.set_title("SVM plot")
     axis2.plot(hyperTree, accTree, color="red", linestyle="-.")
     axis2.set_title("Tree plot")
