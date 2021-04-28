200178d1c2c27c17f24bd443d5b9edba2de7336a
everet12 <54336351+everet12@users.noreply.github.com>
2021-04-28 12:41:38 -0400

    Update HyperVsAcc.py

--- HyperVsAcc.py
+++ HyperVsAcc.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plt
 
-def run(hyperList, accList):
-    plt.plot(hyperList, accList)
-    plt.xlabel("Hyperparameter Value")
-    plt.ylabel("Accuracy")
+def run(hyperSVM, accSVM, hyperTree, accTree):
+    fig1, (axis1, axis2) = plt.subplots(1, 2)
+    fig1.suptitle('Hyperparameter vs. Accuracy')
+    axis1.plot(hyperSVM, accSVM, color="blue", linestyle="-.")
+    axis1.set_title("SVM plot")
+    axis2.plot(hyperTree, accTree, color="red", linestyle="-.")
+    axis2.set_title("Tree plot")
     plt.show()
