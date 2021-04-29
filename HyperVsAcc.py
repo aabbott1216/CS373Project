837fd3b10ba2d5d1ef5881ce159cb4efc74f1f46
Aidan Abbott <77586827+aabbott1216@users.noreply.github.com>
2021-04-29 17:39:47 -0400

    Updated to be less cluttered

--- HyperVsAcc.py
+++ HyperVsAcc.py
@@ -11,26 +11,34 @@ def run(hyperSvm, accSvm, hyperTree, accTree, title):
     fig1, (axis1, axis2) = plt.subplots(1, 2)
     fig1.suptitle('Hyperparameter vs. Accuracy')
     axis1.plot(hyperSvm, accSvm, color="blue", linestyle="-.")
+    axis1.scatter(hyperSvm, accSvm, color='black')
     axis1.set_title("SVM plot")
+    axis1.set_xlabel('Hyperparameter value')
+    axis1.set_ylabel('Accuracy')
 
-    for x, y in zip(hyperSvm, accSvm):
-        label = "{:.1f}".format(x)
-        axis1.annotate(label,
-                       (x, y),
-                       textcoords="offset points",
-                       xytext=(15, -20),
-                       ha='center',
-                       arrowprops=dict(arrowstyle="->"))
+    # Annotating points with hyperparameter value
+    # for x, y in zip(hyperSvm, accSvm):
+    #     label = "{:.1f}".format(x)
+    #     axis1.annotate(label,
+    #                    (x, y),
+    #                    textcoords="offset points",
+    #                    xytext=(15, -20),
+    #                    ha='center',
+    #                    arrowprops=dict(arrowstyle="->"))
 
     axis2.plot(hyperTree, accTree, color="red", linestyle="-.")
     axis2.set_title("Tree plot")
+    axis2.scatter(hyperTree, accTree, color='black')
+    axis2.set_xlabel('Hyperparameter value')
+    axis2.set_ylabel('Accuracy')
 
-    for x, y in zip(hyperTree, accTree):
-        label = "{:.2f}".format(x)
-        axis2.annotate(label,
-                       (x, y),
-                       textcoords="offset points",
-                       xytext=(15, 20),
-                       ha='center',
-                       arrowprops=dict(arrowstyle="->"))
+    # Annotating points with hyperparameter value
+    # for x, y in zip(hyperTree, accTree):
+    #     label = "{:.2f}".format(x)
+    #     axis2.annotate(label,
+    #                    (x, y),
+    #                    textcoords="offset points",
+    #                    xytext=(15, 20),
+    #                    ha='center',
+    #                    arrowprops=dict(arrowstyle="->"))
     plt.show()
