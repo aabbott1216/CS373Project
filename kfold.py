a0fa08613163113569e9f343f02555b81e845ef4
Aidan Abbott <77586827+aabbott1216@users.noreply.github.com>
2021-04-29 14:40:35 -0400

    final update??

--- kfold.py
+++ kfold.py
@@ -14,12 +14,13 @@ from HyperVsAcc import run as acc_plot
 
 
 def run(heart, alg):
+    # Separate dataset into features X and label y
     y = heart['target']
     X = heart.drop(labels='target', axis=1)
+
+    # Initialize k folds, hyperparameters, and dataframe for storing values
     n = y.shape[0]
     k = 15
-    z_svm = [0]*k
-    z_tree = [0]*k
     svm_hyperparam = [0.1, 0.2, 0.5, 0.7, 1, 5, 10,
                       20, 40, 60, 80, 100, 120, 140, 160, 180]
     tree_hyperparam = [0, 0.01, 0.03, .05, 0.75, .1,
@@ -29,6 +30,7 @@ def run(heart, alg):
     tree_df = pd.DataFrame(
         columns=['gini', 'Accuracy', 'Sensitivity', 'Specificity', 'Fold'])
 
+    # Iterate over 15 folds separating data into training and testing sets
     for i in range(k):
         T = range(int(n*(float(i)/k)), int(n*(float(i)+1)/k))
         T_set = set(T)
@@ -42,6 +44,7 @@ def run(heart, alg):
         Xtest = X[T_list[0]:T_list[-1]]
         ytest = y[T_list[0]:T_list[-1]]
 
+        # SVM
         if "svm" in alg:
             # Store hyperparameter accuracies
             hyper_accs_svm = [0]*len(svm_hyperparam)
@@ -54,6 +57,7 @@ def run(heart, alg):
                 pred_length = len(prediction_svm)
                 sens, spec = get_sens_spec(ytest, prediction_svm)
 
+                # Compute accuracy and store information in dataframe
                 for ii in range(len(prediction_svm)):
                     if prediction_svm[ii] == ytest[ii]:
                         hyper_accs_svm[iii] += 1.0
@@ -62,9 +66,12 @@ def run(heart, alg):
                 svm_df = svm_df.append(
                     temp_df)
 
+        # Decision Tree
         if "tree" in alg:
             # Store hyperparameter accuracies
             hyper_accs_tree = [0]*len(tree_hyperparam)
+
+            # For each hyperparameter, make a new model with that hyperparameter value and get the accuracy, sensitivity, and specificity to store in svm_df
             for iii, hyperparam in enumerate(tree_hyperparam):
                 tree = tree_run(Xtrain, ytrain, hyperparam)
                 prediction_tree = list(tree_pred(tree, Xtest))
@@ -72,6 +79,7 @@ def run(heart, alg):
                 pred_length = len(prediction_tree)
                 sens, spec = get_sens_spec(ytest, prediction_tree)
 
+                # Compute accuracy and store information in dataframe
                 for ii in range(len(prediction_tree)):
                     if prediction_tree[ii] == ytest[ii]:
                         hyper_accs_tree[iii] += 1
@@ -81,13 +89,16 @@ def run(heart, alg):
                     temp_df)
 
     # ROC plot data
-    mean_svm_df = (svm_df.groupby('C').mean()).reset_index()
-    mean_tree_df = (tree_df.groupby('Gini').mean()).reset_index()
     roc_plot(svm_df['Sensitivity'], svm_df['Specificity'],
              tree_df['Sensitivity'], tree_df['Specificity'], "kfold")
+
+    # Calculate mean Accuracy, Sensitivity, and Specificity for each C value across all folds
+    mean_svm_df = (svm_df.groupby('C').mean()).reset_index()
+    mean_tree_df = (tree_df.groupby('Gini').mean()).reset_index()
 
     # Accuracy vs hyperparam plot
     acc_plot(mean_svm_df['C'], mean_svm_df['Accuracy'],
              mean_tree_df['Gini'], mean_tree_df['Accuracy'], "kfold")
 
-    return (np.mean(z_svm), np.mean(z_tree))
+    # return mean Accuracy, Sensitivity, and Specificity for each C value across all folds
+    return [mean_svm_df, mean_tree_df]
