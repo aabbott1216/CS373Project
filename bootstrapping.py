ce02b661756614f8bb9195ea91f79ff30da3c8d2
Aidan Abbott <77586827+aabbott1216@users.noreply.github.com>
2021-04-29 14:54:07 -0400

    final update??

--- bootstrapping.py
+++ bootstrapping.py
@@ -16,25 +16,24 @@ from HyperVsAcc import run as acc_plot
 
 def run(heart, alg):
 
-    # Forms needed matrices.
+    # Separate data into X features and y label
     y = heart['target']
     X = heart.drop(labels='target', axis=1)
+
+    # Initialize bootstrap rounds, hyperparameters, and dataframe to store values
     n = y.shape[0]
     b = 30
-    z_svm = [0]*b
-    z_tree = [0]*b
     svm_hyperparam = [0.1, 0.2, 0.5, 0.7, 1, 5, 10,
                       20, 40, 60, 80, 100, 120, 140, 160, 180]
     tree_hyperparam = [0, 0.01, 0.03, .05, 0.75, .1,
                        0.125, .15, 0.175, .20, .25, .30, .35, .40, .45, .50]
     svm_df = pd.DataFrame(
-        columns=['C', 'Accuracy', 'Sensitivity', 'Specificity', 'Fold'])
+        columns=['C', 'Accuracy', 'Sensitivity', 'Specificity', 'Round'])
     tree_df = pd.DataFrame(
-        columns=['gini', 'Accuracy', 'Sensitivity', 'Specificity', 'Fold'])
+        columns=['gini', 'Accuracy', 'Sensitivity', 'Specificity', 'Round'])
 
     # Runs the bootstrapping cross-validation.
     for i in range(b):
-
         # Determines random set.
         u = np.zeros(n)
         S = set()
@@ -56,13 +55,6 @@ def run(heart, alg):
 
         # Bootstrapping for SVM.
         if "svm" in alg:
-
-            # LEGACY CODE
-            # classifier = svm_run(X_train,y_train)
-            # for t in T:
-            #     if y[t] != svm_pred(classifier, X[t:t+1,:].T):
-            #         z[i] = z[i]+1
-
             # Store hyperparameter accuracies.
             hyper_accs = [0]*len(svm_hyperparam)
 
@@ -74,25 +66,22 @@ def run(heart, alg):
                 ytest = list(ytest)
                 pred_length = len(prediction_svm)
                 sens, spec = get_sens_spec(ytest, prediction_svm)
+
+                # Compute accuracy and store information in dataframe
                 for ii in range(len(prediction_svm)):
                     if prediction_svm[ii] == ytest[ii]:
                         hyper_accs[iii] += 1.0
                 temp_df = pd.DataFrame([[hyperparam, hyper_accs[iii]/pred_length, sens, spec, i]],
-                                       columns=['C', 'Accuracy', 'Sensitivity', 'Specificity', 'Fold'])
+                                       columns=['C', 'Accuracy', 'Sensitivity', 'Specificity', 'Round'])
                 svm_df = svm_df.append(temp_df)
 
         # Bootstrapping for decision tree.
         if "tree" in alg:
-
-            # LEGACY CODE
-            # tree = tree_run(X_train,y_train)
-            # for t in T:
-            #     if y[t] != tree_pred(tree, X[t:t+1,:].T):
-            #         z[i] = z[i]+1
-
             # Store hyperparameter accuracies
             hyper_accs_tree = [0]*len(tree_hyperparam)
 
+            # For each hyperparameter, make a new model with that hyperparameter value and get the accuracy, sensitivity, and specificity to store in tree_df.
+            # Essentially copied from the kfold.py file.
             for iii, hyperparam in enumerate(tree_hyperparam):
                 tree = tree_run(Xtrain, ytrain, hyperparam)
                 prediction_tree = list(tree_pred(tree, Xtest))
@@ -100,27 +89,25 @@ def run(heart, alg):
                 pred_length = len(prediction_tree)
                 sens, spec = get_sens_spec(ytest, prediction_tree)
 
+                # Compute accuracy and store information in dataframe
                 for ii in range(len(prediction_tree)):
                     if prediction_tree[ii] == ytest[ii]:
                         hyper_accs_tree[iii] += 1
                 temp_df = pd.DataFrame([[hyperparam, hyper_accs_tree[iii]/pred_length, sens, spec, i]],
-                                       columns=['gini', 'Accuracy', 'Sensitivity', 'Specificity', 'Fold'])
+                                       columns=['gini', 'Accuracy', 'Sensitivity', 'Specificity', 'Round'])
                 tree_df = tree_df.append(temp_df)
 
-        # z[i] = z[i]/len(T)
-        # z = z.reshape(z.shape[0],1)
-        # z_svm[i] /= pred_length
-        # z_tree[i] /= pred_length
-
     # ROC plot data
-    mean_svm_df = (svm_df.groupby('C').mean()).reset_index()
-    mean_tree_df = (tree_df.groupby('gini').mean()).reset_index()
     roc_plot(svm_df['Sensitivity'], svm_df['Specificity'],
              tree_df['Sensitivity'], tree_df['Specificity'], "boot")
-             
+
+    # Calculate mean Accuracy, Sensitivity, and Specificity for each C value across all samples
+    mean_svm_df = (svm_df.groupby('C').mean()).reset_index()
+    mean_tree_df = (tree_df.groupby('gini').mean()).reset_index()
+
     # Accuracy vs hyperparam plot
     acc_plot(mean_svm_df['C'], mean_svm_df['Accuracy'],
              mean_tree_df['gini'], mean_tree_df['Accuracy'], "boot")
 
-    # return z
-    return (np.mean(z_svm), np.mean(z_tree))
+    # return average accuracy, sensitivity, specificity across all 30 rounds
+    return [mean_svm_df, mean_tree_df]
